45c38fc7d01327b646f502c6f365aeb4119489da
Feiya Ou <fo2827@mun.ca>
2018-11-25 21:37:43 -0330

    network manager class created
    mun student class helper network manager helper class

---
+++ COMP4768FinalProject/MSCHNetworkManager.h
@@ -0,0 +1,17 @@
+//
+//  MSCHNetworkManager.h
+//  COMP4768FinalProject
+//
+//  Created by Feiya Ou on 2018-11-25.
+//  Copyright © 2018 Feiya Ou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MSCHNetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ COMP4768FinalProject/MSCHNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  MSCHNetworkManager.m
+//  COMP4768FinalProject
+//
+//  Created by Feiya Ou on 2018-11-25.
+//  Copyright © 2018 Feiya Ou. All rights reserved.
+//
+
+#import "MSCHNetworkManager.h"
+
+@implementation MSCHNetworkManager
+
+@end
