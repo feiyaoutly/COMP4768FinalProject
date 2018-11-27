b9509d3a2b216d208763ceeedf96a27008f34c25
Feiya Ou <fo2827@mun.ca>
2018-11-26 20:44:19 -0330

    MSCHPersistenceManager class created

---
+++ COMP4768FinalProject/MSCHPersistenceManager.m
@@ -0,0 +1,13 @@
+//
+//  MSCHPersistenceManager.m
+//  COMP4768FinalProject
+//
+//  Created by Feiya Ou on 2018-11-26.
+//  Copyright © 2018 Feiya Ou. All rights reserved.
+//
+
+#import "MSCHPersistenceManager.h"
+
+@implementation MSCHPersistenceManager
+
+@end
