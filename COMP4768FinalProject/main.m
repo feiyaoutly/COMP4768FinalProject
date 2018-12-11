bdcf00135658e6b2f28fb608fb07c3ac22aa3e08
Feiya Ou <fo2827@mun.ca>
2018-12-11 19:05:20 -0330

    add our info

--- COMP4768FinalProject/main.m
+++ COMP4768FinalProject/main.m
@@ -1,7 +1,9 @@
 //
 //  main.m
 //  COMP4768FinalProject
-//
+//  Feiya Ou, 201227089
+//  He Zhang, 201413010
+//  Wenrui Zhen, 201409489
 //  Created by Feiya Ou on 2018-11-06.
 //  Copyright © 2018 Feiya Ou. All rights reserved.
 //
